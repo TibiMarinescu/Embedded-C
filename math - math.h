2c2199eb2e7bbef8b4c30f4b36cfda11376a724c
TibiMarinescu <64827320+TibiMarinescu@users.noreply.github.com>
2022-02-04 15:48:49 +0200

    Create math - math.h

---
+++ math - math.h
@@ -0,0 +1,18 @@
+/*
+ * math.h
+ *
+ *  Created on: Feb 4, 2022
+ *      Author: tibi
+ */
+
+#ifndef MATH_H_
+#define MATH_H_
+
+/* These are also called as exposed prototypes.*/
+int math_add(int n1, int n2);
+int math_sub(int n1, int n2);
+long long math_mul(int n1, int n2);
+float math_div(int n1, int n2);
+
+
+#endif /* MATH_H_ */
